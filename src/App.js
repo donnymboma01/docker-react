082fea9fd02c32350c36f8eab3ec926eb49770ab
donnymboma01 <donnymboma01@gmail.com>
2023-09-13 20:23:46 +0200

    feat: just a new paragraph

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome everyone to the wonderful world of Docker and Kubernetes!</h1>
+        <h1>Welcome everyone to the wonderful world of Docker and Kubernetes! </h1>
         <p>Ici je vais écrire en francais juste pour observer les changements qui se font du coté de l'application</p>
         <p>J'ai comme l'impression qu'il y a un petit moment avant que le changement soient pris en compte en React.</p>
+        <p>je compte commencer kubernetes dans moins d'un mois</p>
       </header>
     </div>
   );
