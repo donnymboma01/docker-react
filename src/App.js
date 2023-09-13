c0c48d758482ebf79632bcd3dbb2c1cb30cdc44d
donnymboma01 <donnymboma01@gmail.com>
2023-09-13 20:21:14 +0200

    fix : nothing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome everyone to the wonderful world of Docker and Kubernetes !</h1>
+        <h1>Welcome everyone to the wonderful world of Docker and Kubernetes!</h1>
         <p>Ici je vais écrire en francais juste pour observer les changements qui se font du coté de l'application</p>
         <p>J'ai comme l'impression qu'il y a un petit moment avant que le changement soient pris en compte en React.</p>
       </header>
